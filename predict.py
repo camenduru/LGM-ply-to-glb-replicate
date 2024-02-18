f1fbbcf1c06a3b8e84c249290a8466f2b3d5fe84
camenduru <54370274+camenduru@users.noreply.github.com>
2024-02-18 13:55:09 +0300

    test

--- predict.py
+++ predict.py
@@ -4,6 +4,11 @@ from typing import List
 import sys, shutil, subprocess
 sys.path.append('/content/LGM')
 os.chdir('/content/LGM')
+current_path = os.environ.get('PATH', '')
+print(current_path)
+new_path = current_path + os.pathsep + '/usr/local/cuda/bin'
+os.environ['PATH'] = new_path
+print(new_path)
 
 class Predictor(BasePredictor):
     def predict(
